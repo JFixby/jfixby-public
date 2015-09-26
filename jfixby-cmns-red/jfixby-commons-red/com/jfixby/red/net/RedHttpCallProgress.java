3f24120b4622bc4bd071608cba96d93f9a55fbab
J Fixby <github@jfixby.com>
2015-09-26 18:02:25 +0300

    +object

--- jfixby-cmns-red/jfixby-commons-red/com/jfixby/red/net/RedHttpCallProgress.java
+++ jfixby-cmns-red/jfixby-commons-red/com/jfixby/red/net/RedHttpCallProgress.java
@@ -1,5 +1,8 @@
 package com.jfixby.red.net;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.UnsupportedEncodingException;
 
 import com.jfixby.cmns.api.net.http.HttpCallProgress;
@@ -23,6 +26,14 @@ public class RedHttpCallProgress implements HttpCallProgress {
 		String input_data = new String(data, encoding_string);
 
 		return input_data;
+	}
+
+	@Override
+	public Object readObject() throws IOException, ClassNotFoundException {
+		ByteArrayInputStream bis = new ByteArrayInputStream(data);
+		ObjectInputStream ois = new ObjectInputStream(bis);
+		Object object = ois.readObject();
+		return object;
 	}
 
 }
