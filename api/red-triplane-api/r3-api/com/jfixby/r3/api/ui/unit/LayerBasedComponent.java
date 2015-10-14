0e4d21c4e493c0612735798f4722e7c7b8813eac
J Fixby <github@jfixby.com>
2015-10-14 16:11:10 +0200

    ...

--- api/red-triplane-api/r3-api/com/jfixby/r3/api/ui/unit/LayerBasedComponent.java
+++ api/red-triplane-api/r3-api/com/jfixby/r3/api/ui/unit/LayerBasedComponent.java
@@ -1,8 +1,9 @@
 package com.jfixby.r3.api.ui.unit;
 
+import com.jfixby.r3.api.ui.unit.layer.Component;
 import com.jfixby.r3.api.ui.unit.layer.Layer;
 
-public interface LayerBasedComponent {
+public interface LayerBasedComponent extends Component {
 
 	public Layer getRoot();
 
