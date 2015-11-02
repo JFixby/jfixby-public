d9c372cb81e7b5f16244df7c05e5137c1c90363c
J Fixby <github@jfixby.com>
2015-11-02 13:48:49 +0100

    split ImageGWT to jfixby-cv

--- util/jfixby-tool-psd-to-scene2d/src/com/jfixby/tool/psd2scene2d/PSDRepacker.java
+++ util/jfixby-tool-psd-to-scene2d/src/com/jfixby/tool/psd2scene2d/PSDRepacker.java
@@ -15,11 +15,11 @@ import com.jfixby.cmns.api.filesystem.File;
 import com.jfixby.cmns.api.filesystem.FileSystem;
 import com.jfixby.cmns.api.filesystem.cache.FileCache;
 import com.jfixby.cmns.api.filesystem.cache.TempFolder;
-import com.jfixby.cmns.api.image.ImageProcessing;
 import com.jfixby.cmns.api.json.Json;
 import com.jfixby.cmns.api.log.L;
 import com.jfixby.cmns.api.math.FloatMath;
 import com.jfixby.cmns.api.sys.Sys;
+import com.jfixby.cv.api.gwt.ImageGWT;
 import com.jfixby.psd.unpacker.api.PSDFileContent;
 import com.jfixby.psd.unpacker.api.PSDLayer;
 import com.jfixby.psd.unpacker.api.PSDRaster;
@@ -43,8 +43,7 @@ import com.jfixby.tools.gdx.texturepacker.api.TexturePackingSpecs;
 
 public class PSDRepacker {
 
-	public static void repackPSD(PSDRepackSettings settings,
-			PSDRepackingStatus handler) throws IOException {
+	public static void repackPSD(PSDRepackSettings settings, PSDRepackingStatus handler) throws IOException {
 
 		File psd_file = settings.getPSDFile();
 		AssetID package_name = settings.getPackageName();
@@ -72,23 +71,19 @@ public class PSDRepacker {
 		File atlas_output;
 		File scene2d_output;
 
-		scene2d_output = repacking_output.child(package_name.child(
-				Scene2DPackage.SCENE2D_PACKAGE_FILE_EXTENSION).toString());
+		scene2d_output = repacking_output.child(package_name.child(Scene2DPackage.SCENE2D_PACKAGE_FILE_EXTENSION).toString());
 		scene2d_output.makeFolder();
 		related_folders.add(scene2d_output);
-		scene2d_output = scene2d_output
-				.child(PackageDescriptor.PACKAGE_CONTENT_FOLDER);
+		scene2d_output = scene2d_output.child(PackageDescriptor.PACKAGE_CONTENT_FOLDER);
 		scene2d_output.makeFolder();
 		scene2d_output.clearFolder();
 
 		Map<PSDLayer, AssetID> raster_names = JUtils.newMap();
 
-		PSDFileContent layers_structure = extractLayerStructures(psd_file,
-				raster_names, package_name);
+		PSDFileContent layers_structure = extractLayerStructures(psd_file, raster_names, package_name);
 
 		L.d("---[Packing Layers Structure]--------------------------------------------");
-		ConversionResult pack_result = packLayers(layers_structure,
-				package_name, scene2d_output, raster_names
+		ConversionResult pack_result = packLayers(layers_structure, package_name, scene2d_output, raster_names
 
 		);
 
@@ -98,17 +93,13 @@ public class PSDRepacker {
 		boolean raster_produced = false;
 		boolean save_raster = true;
 		Map<PSDLayer, File> layer_to_file_mapping = JUtils.newMap();
-		raster_produced = saveRaster(psd_file, raster_names, used_raster,
-				raster_folder, package_name, save_raster,
-				layer_to_file_mapping, pack_result);
+		raster_produced = saveRaster(psd_file, raster_names, used_raster, raster_folder, package_name, save_raster, layer_to_file_mapping, pack_result);
 
 		if (!ignore_atlas && raster_produced) {
 			L.d("---[Decomposing Raster]--------------------------------------------");
 			File tiling_folder = temp_folder.child("tiling");
 			tiling_folder.makeFolder();
-			Collection<TextureSlicingResult> structures = decomposeRaster(
-					layer_to_file_mapping, tiling_folder, max_texture_size,
-					margin);
+			Collection<TextureSlicingResult> structures = decomposeRaster(layer_to_file_mapping, tiling_folder, max_texture_size, margin);
 			raster_folder.delete();
 
 			SlicesCompositionsContainer container = new SlicesCompositionsContainer();
@@ -118,41 +109,32 @@ public class PSDRepacker {
 			for (TextureSlicingResult combo : structures) {
 				SlicesCompositionInfo composition = combo.getTilesComposition();
 				container.content.addElement(composition);
-				packed_structures.add(Names
-						.newAssetID(composition.composition_asset_id_string));
+				packed_structures.add(Names.newAssetID(composition.composition_asset_id_string));
 				requred_rasters.addAll(combo.listProducedTiles());
 			}
 
 			if (container.content.size() > 0) {
-				AssetID sctruct_package_name = package_name.child("psd").child(
-						TextureSlicerSpecs.TILE_MAP_FILE_EXTENSION);
+				AssetID sctruct_package_name = package_name.child("psd").child(TextureSlicerSpecs.TILE_MAP_FILE_EXTENSION);
 
 				String struct_pkg_name = sctruct_package_name.toString();
 				File container_file = repacking_output.child(struct_pkg_name);
 				related_folders.add(container_file);
-				container_file = container_file
-						.child(PackageDescriptor.PACKAGE_CONTENT_FOLDER);
+				container_file = container_file.child(PackageDescriptor.PACKAGE_CONTENT_FOLDER);
 				container_file.makeFolder();
 
-				container_file = container_file.child(sctruct_package_name
-						.toString());
+				container_file = container_file.child(sctruct_package_name.toString());
 
 				String data = Json.serializeToString(container);
 				container_file.writeString(data);
 
-				producePackageDescriptor(container_file.parent().parent(),
-						StandardPackageFormats.RedTriplane.TiledRaster, "1.0",
-						packed_structures, requred_rasters,
-						container_file.getName());
+				producePackageDescriptor(container_file.parent().parent(), StandardPackageFormats.RedTriplane.TiledRaster, "1.0", packed_structures, requred_rasters, container_file.getName());
 
 			}
 
 			L.d("---[Packing Atlas]--------------------------------------------");
 			File atlas_folder = temp_folder.child("atlas");
 			atlas_folder.makeFolder();
-			AtlasPackingResult atlas_result = packAtlas(atlas_folder,
-					tiling_folder, package_name.child("psd").child("raster")
-							.toString());
+			AtlasPackingResult atlas_result = packAtlas(atlas_folder, tiling_folder, package_name.child("psd").child("raster").toString());
 
 			atlas_result.print();
 
@@ -161,8 +143,7 @@ public class PSDRepacker {
 
 			tiling_folder.delete();
 
-			atlas_output = repacking_output.child(atlas_name).child(
-					PackageDescriptor.PACKAGE_CONTENT_FOLDER);
+			atlas_output = repacking_output.child(atlas_name).child(PackageDescriptor.PACKAGE_CONTENT_FOLDER);
 			atlas_output.makeFolder();
 			related_folders.add(atlas_output);
 			atlas_output.clearFolder();
@@ -174,9 +155,7 @@ public class PSDRepacker {
 			packed_rasters.addAll(requred_rasters);
 			packed_rasters.addAll(atlas_result.listPackedAssets());
 
-			producePackageDescriptor(atlas_output.parent(),
-					StandardPackageFormats.libGDX.Atlas_GWT, "1.0",
-					packed_rasters, JUtils.newList(), atlas_name);
+			producePackageDescriptor(atlas_output.parent(), StandardPackageFormats.libGDX.Atlas_GWT, "1.0", packed_rasters, JUtils.newList(), atlas_name);
 
 			// requred_rasters.print("requred_rasters");
 			// packed_rasters.print("packed_rasters");
@@ -195,9 +174,7 @@ public class PSDRepacker {
 
 	}
 
-	static private PSDFileContent extractLayerStructures(File psd_file,
-			Map<PSDLayer, AssetID> raster_names, AssetID package_name)
-			throws IOException {
+	static private PSDFileContent extractLayerStructures(File psd_file, Map<PSDLayer, AssetID> raster_names, AssetID package_name) throws IOException {
 		int k = 0;
 
 		PSDUnpackingParameters specs = PSDUnpacker.newUnpackingSpecs();
@@ -209,18 +186,14 @@ public class PSDRepacker {
 
 		for (int i = 0; i < rasters.size(); i++) {
 			PSDLayer element = rasters.getElementAt(i);
-			AssetID raster_name = Names.newAssetID(package_name
-					+ ".psd.raster_" + k);
+			AssetID raster_name = Names.newAssetID(package_name + ".psd.raster_" + k);
 			raster_names.put(element, raster_name);
 			k++;
 		}
 		return result;
 	}
 
-	static private void producePackageDescriptor(File output_folder,
-			String format, String version, Collection<AssetID> provisions,
-			Collection<AssetID> dependencies, String root_file_name)
-			throws IOException {
+	static private void producePackageDescriptor(File output_folder, String format, String version, Collection<AssetID> provisions, Collection<AssetID> dependencies, String root_file_name) throws IOException {
 
 		PackageDescriptor descriptor = new PackageDescriptor();
 		descriptor.format = format;
@@ -234,30 +207,25 @@ public class PSDRepacker {
 		}
 
 		descriptor.root_file_name = root_file_name;
-		File output_file = output_folder
-				.child(PackageDescriptor.PACKAGE_DESCRIPTOR_FILE_NAME);
+		File output_file = output_folder.child(PackageDescriptor.PACKAGE_DESCRIPTOR_FILE_NAME);
 
 		output_file.writeString(Json.serializeToString(descriptor));
 
 	}
 
-	static private ConversionResult packLayers(PSDFileContent layers_structure,
-			AssetID package_name, File final_output,
-			Map<PSDLayer, AssetID> raster_names) throws IOException {
+	static private ConversionResult packLayers(PSDFileContent layers_structure, AssetID package_name, File final_output, Map<PSDLayer, AssetID> raster_names) throws IOException {
 
 		AssetID package_prefix = package_name;
 
 		Scene2DPackage container = new Scene2DPackage();
 		PSDRootLayer root = layers_structure.getRootlayer();
-		ConversionResult result = PSDtoScene2DConverter.convert(container,
-				package_prefix, root, raster_names);
+		ConversionResult result = PSDtoScene2DConverter.convert(container, package_prefix, root, raster_names);
 
 		// AssetID asset_id = Names.newAssetID(package_prefix);
 
 		// SceneStructure structure = container.structures.get(0);
 		// structure.structure_name = package_prefix;
-		String root_file_name = package_prefix.child(
-				Scene2DPackage.SCENE2D_PACKAGE_FILE_EXTENSION).toString();
+		String root_file_name = package_prefix.child(Scene2DPackage.SCENE2D_PACKAGE_FILE_EXTENSION).toString();
 		String data = Json.serializeToString(container);
 		File file = final_output.child(root_file_name);
 		file.writeString(data);
@@ -267,22 +235,18 @@ public class PSDRepacker {
 		List<AssetID> provisions = JUtils.newList();
 
 		for (int i = 0; i < container.structures.size(); i++) {
-			AssetID element_id = Names
-					.newAssetID(container.structures.get(i).structure_name);
+			AssetID element_id = Names.newAssetID(container.structures.get(i).structure_name);
 			provisions.add(element_id);
 		}
 
 		Collection<AssetID> requred_assets = result.listAllRequredAssets();
-		producePackageDescriptor(descriptor,
-				Scene2DPackage.SCENE2D_PACKAGE_FORMAT, "1.0", provisions,
-				requred_assets, root_file_name);
+		producePackageDescriptor(descriptor, Scene2DPackage.SCENE2D_PACKAGE_FORMAT, "1.0", provisions, requred_assets, root_file_name);
 
 		return result;
 
 	}
 
-	static private AtlasPackingResult packAtlas(File atlas_folder,
-			File sprites, String atlas_file_name) throws IOException {
+	static private AtlasPackingResult packAtlas(File atlas_folder, File sprites, String atlas_file_name) throws IOException {
 
 		TexturePackingSpecs specs = TexturePacker.newPackingSpecs();
 
@@ -298,9 +262,7 @@ public class PSDRepacker {
 		return result;
 	}
 
-	static private List<TextureSlicingResult> decomposeRaster(
-			Map<PSDLayer, File> layer_to_file_mapping, File tiling_folder,
-			int max_texture_size, int margin) throws IOException {
+	static private List<TextureSlicingResult> decomposeRaster(Map<PSDLayer, File> layer_to_file_mapping, File tiling_folder, int max_texture_size, int margin) throws IOException {
 		List<TextureSlicingResult> results = JUtils.newList();
 		for (int i = 0; i < layer_to_file_mapping.size(); i++) {
 			PSDLayer layer_info = layer_to_file_mapping.getKeyAt(i);
@@ -312,22 +274,18 @@ public class PSDRepacker {
 			File png_file = layer_to_file_mapping.get(layer_info);
 			if (diag > max_texture_size) {
 				// decompose
-				TextureSlicingResult result = decomposeSprite(png_file,
-						tiling_folder, margin, max_texture_size);
+				TextureSlicingResult result = decomposeSprite(png_file, tiling_folder, margin, max_texture_size);
 				results.add(result);
 			} else {
 				// copy as is
 				File file_to_copy = png_file;
-				tiling_folder.getFileSystem().copyFileToFolder(file_to_copy,
-						tiling_folder);
+				tiling_folder.getFileSystem().copyFileToFolder(file_to_copy, tiling_folder);
 			}
 		}
 		return results;
 	}
 
-	static private TextureSlicingResult decomposeSprite(File png_file_path,
-			File tiling_folder, int margin, int max_texture_size)
-			throws IOException {
+	static private TextureSlicingResult decomposeSprite(File png_file_path, File tiling_folder, int margin, int max_texture_size) throws IOException {
 
 		File output_folder = tiling_folder;
 		output_folder.makeFolder();
@@ -339,8 +297,7 @@ public class PSDRepacker {
 		specs.setTileHeight(max_texture_size - 2 * margin);
 		specs.setMargin(margin);
 
-		String asset_name = png_file_path.getName().substring(0,
-				png_file_path.getName().length() - ".png".length());
+		String asset_name = png_file_path.getName().substring(0, png_file_path.getName().length() - ".png".length());
 
 		specs.setNameSpacePrefix(Names.newAssetID(asset_name));
 		specs.setOutputFolder(tiling_folder);
@@ -349,12 +306,8 @@ public class PSDRepacker {
 		return result;
 	}
 
-	static private boolean saveRaster(File package_root_file,
-			Map<PSDLayer, AssetID> raster_names,
-			Collection<AssetID> used_raster, File output_folder,
-			AssetID package_name, boolean save_raster,
-			Map<PSDLayer, File> layer_to_file_mapping,
-			ConversionResult pack_result) throws IOException {
+	static private boolean saveRaster(File package_root_file, Map<PSDLayer, AssetID> raster_names, Collection<AssetID> used_raster, File output_folder, AssetID package_name, boolean save_raster, Map<PSDLayer, File> layer_to_file_mapping,
+		ConversionResult pack_result) throws IOException {
 		boolean raster_produced = false;
 		for (int i = 0; i < raster_names.size(); i++) {
 
@@ -372,17 +325,13 @@ public class PSDRepacker {
 
 			String png_file_name = raster_name + ".png";
 
-			saveRaster(png_file_name, layer, output_folder, save_raster,
-					layer_to_file_mapping, pack_result);
+			saveRaster(png_file_name, layer, output_folder, save_raster, layer_to_file_mapping, pack_result);
 
 		}
 		return raster_produced;
 	}
 
-	static private void saveRaster(String png_file_name, PSDLayer layer,
-			File output_folder, boolean save_raster,
-			Map<PSDLayer, File> layer_to_file_mapping,
-			ConversionResult pack_result) throws IOException {
+	static private void saveRaster(String png_file_name, PSDLayer layer, File output_folder, boolean save_raster, Map<PSDLayer, File> layer_to_file_mapping, ConversionResult pack_result) throws IOException {
 
 		File output_file = output_folder.child(png_file_name);
 
@@ -393,24 +342,20 @@ public class PSDRepacker {
 
 		JUtils.checkNull("java_image", java_image);
 
-		SceneStructurePackingResult result = pack_result
-				.getStrucutreResultByLayer(layer);
+		SceneStructurePackingResult result = pack_result.getStrucutreResultByLayer(layer);
 
 		double scale_factor = result.getScaleFactor();
 
 		Image out;
 		if (scale_factor != 1) {
-			out = java_image.getScaledInstance(
-					(int) (java_image.getWidth() * scale_factor),
-					(int) (java_image.getHeight() * scale_factor),
-					BufferedImage.SCALE_SMOOTH);
+			out = java_image.getScaledInstance((int) (java_image.getWidth() * scale_factor), (int) (java_image.getHeight() * scale_factor), BufferedImage.SCALE_SMOOTH);
 		} else {
 			out = java_image;
 		}
 
 		if (save_raster) {
 			L.d("writing: " + output_file);
-			ImageProcessing.writeJavaFile(out, output_file, "png");
+			ImageGWT.writeJavaFile(out, output_file, "png");
 		}
 
 	}
