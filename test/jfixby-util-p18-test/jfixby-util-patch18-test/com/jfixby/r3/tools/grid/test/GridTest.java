3bc2bbe6aebbedb3906ec68a29a5093a2f36f643
J Fixby <github@jfixby.com>
2015-12-04 19:09:25 +0100

    ...

--- test/jfixby-util-p18-test/jfixby-util-patch18-test/com/jfixby/r3/tools/grid/test/GridTest.java
+++ test/jfixby-util-p18-test/jfixby-util-patch18-test/com/jfixby/r3/tools/grid/test/GridTest.java
@@ -1,18 +1,27 @@
 package com.jfixby.r3.tools.grid.test;
 
+import com.jfixby.cmns.api.md5.MD5;
+import com.jfixby.cmns.desktop.DesktopAssembler;
 import com.jfixby.examples.wdgs.WDGS_P18_Palette;
 import com.jfixby.r3.ext.api.patch18.Grid;
+import com.jfixby.r3.ext.api.patch18.GridFactory;
 import com.jfixby.r3.ext.api.patch18.GridSpecs;
+import com.jfixby.r3.ext.api.patch18.P18;
 import com.jfixby.r3.ext.api.patch18.grid.GridBrush;
 import com.jfixby.r3.ext.api.patch18.grid.GridBrushApplicationResult;
-import com.jfixby.util.patch18.red.RedGridFactory;
+import com.jfixby.red.util.md5.AlpaeroMD5;
+import com.jfixby.util.patch18.red.RedP18;
 
 public class GridTest {
 
 	public static void main(String[] args) {
-		Setup.setup();
 
-		RedGridFactory factory = new RedGridFactory();
+		DesktopAssembler.setup();
+
+		MD5.installComponent(new AlpaeroMD5());
+		P18.installComponent(new RedP18());
+
+		GridFactory factory = P18.getGridFactory();
 
 		GridSpecs grid_specs = factory.newGridSpecs();
 		grid_specs.setPalette(WDGS_P18_Palette.PALETTE);
